2e2cd00b71242ab49f3509ef5b21d97c9ee82c36
Frank Rousseau <frank.rousseau@free.fr>
2017-08-08 12:42:53 +0200

    Make index page title configurable

--- zou/app/resources/index.py
+++ zou/app/resources/index.py
@@ -1,10 +1,12 @@
 from flask_restful import Resource
 from zou import __version__
 
+from zou.app.config import APP_NAME
+
 
 class IndexResource(Resource):
     def get(self):
         return {
-            'api': 'Zou API',
+            'api': APP_NAME,
             'version': __version__
         }
